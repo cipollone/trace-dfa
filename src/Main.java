7c10a370f927ce042487aab41470143fd107d5a8
Roberto Cipollone <cipollone.rt@gmail.com>
2018-11-27 15:06:30 +0100

    Reorganized TraceManager
    + Now it can load all traces from a directory
    - Strange dependency with APTA and alone in package.
        Just leaving as it is, for now

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 
-import tracemanager.Test;
+import automata.*;
+import tracemanager.TraceManager;
+
+import java.io.File;
 
 public class Main {
 	public static void main(String args[]) {
 
-		Test.test();
+		APTA<String> apta = TraceManager.parseTracesFiles(new File("traces"));
+		LatexSaver.saveLatexFile(apta, new File("latex/traces_apta.tex"), 3);
 
 	}
 }
